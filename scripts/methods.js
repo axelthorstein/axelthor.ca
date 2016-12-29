03a383f45540b454033830e909d16791115b07ec
Axel Thor <axelthorstein@gmail.com>
2016-12-29 14:21:18 -0500

    Rewrote logic

--- scripts/methods.js
+++ scripts/methods.js
@@ -4,103 +4,73 @@ $(document).ready(function() {
 
     $("#hexagon").click(function() {
         if ($("#hexagon img.outline-slide.1").hasClass("slide-out-left")) {
-            removeSlideClasses("out");
+            removeSlideClasses(0, 1);
+        } else if ($("#hexagon img.outline-slide.3").hasClass("slide-out-left")) {
+            removeSlideClasses(2, 3);
         } else {
             $("#hexagon img.top").toggleClass("transparent");
             $("#hexagon img.bottom").toggleClass("transparent");
-        }
+        };
     });
 
-    $("#nav_onclick .nav-item").click(function() {
+    $("#nav_onclick .development").click(function() {
 
-        if ($("#hexagon img.top").hasClass("transparent") && !$("#hexagon img.outline-slide.1").hasClass("slide-out-left") ) {
-            $("#hexagon img").toggleClass("transparent");
-        } 
+        if ($("#hexagon img.outline-slide.3").hasClass("slide-out-left")) {
+            removeSlideClasses(2, 3);
+        } ;
+        slideInOut("#hexagon img.outline-slide.0", "#hexagon img.outline-slide.1", 0, 1);
+       
+    });
 
-        slideInOut("#hexagon img.outline-slide.1", "#hexagon img.outline-slide.0");
+    $("#nav_onclick .creative").click(function() {
+
+        if ($("#hexagon img.outline-slide.1").hasClass("slide-out-left")) {
+            removeSlideClasses(0, 1);
+        };
+        slideInOut("#hexagon img.outline-slide.2", "#hexagon img.outline-slide.3", 2, 3);
         
     });
 });
 
-function toggleTransparency() {
-    $("#hexagon img").toggleClass("transparent");
-    $("#hexagon a").toggleClass("transparent");
+function checkTransparency() {
+    if ($("#hexagon img.top").hasClass("transparent")) {
+        $("#hexagon img.top").toggleClass("transparent");
+        $("#hexagon img.bottom").toggleClass("transparent");
+    };
 }
 
-function removeSlideClasses(direction) {
+function toggleTransparency(num1, num2) {
+    $("#hexagon img.outline-slide." + num1).toggleClass("transparent");
+    $("#hexagon img.outline-slide." + num2).toggleClass("transparent");
 
-    $("#hexagon img.outline-slide.1").removeClass("slide-out-left");
-    $("#hexagon img.outline-slide.0").removeClass("slide-out-right");
-    $("#hexagon a").addClass("not-active");
-    $("#hexagon img.outline-slide.1").one('otransitionend oTransitionend msTransitionend transitionend',
+    // if (($("#hexagon img.bottom").hasClass("transparent") && !$("#hexagon img.outline-slide." + num1).hasClass("slide-out-left")) {
+        $("#hexagon img.top").toggleClass("transparent");
+        $("#hexagon img.bottom").toggleClass("transparent");
+    // };
+}
+
+function removeSlideClasses(num1, num2) {
+
+    $("#hexagon img.outline-slide." + num2).removeClass("slide-out-left");
+    $("#hexagon img.outline-slide." + num1).removeClass("slide-out-right");
+    $("#hexagon a." + num1).addClass("not-active");
+    $("#hexagon a." + num2).addClass("not-active");
+    $("#hexagon img.outline-slide." + num1).one('otransitionend oTransitionend msTransitionend transitionend',
         function(e) {
-            toggleTransparency();
+            toggleTransparency(num1, num2);
         });
 }
 
-function slideInOut(hex_1, hex_0) {
+function slideInOut(hex_0, hex_1, num1, num2) {
     if (!$(hex_1).hasClass("slide-out-left")) {
 
         $(hex_1).addClass("slide-out-left");
         $(hex_0).addClass("slide-out-right");
-        $("#hexagon a").removeClass("not-active");
-        toggleTransparency();
+        $("#hexagon a." + num1).removeClass("not-active");
+        $("#hexagon a." + num2).removeClass("not-active");
+        toggleTransparency(num1, num2);
 
     } else {
-        removeSlideClasses("out");
+        removeSlideClasses(num1, num2);
     }
 }
-
-// // Logo SVG Fade OnClick
-// $(document).ready(function() {
-
-//     $("#nav_onclick .nav-item").click(function() {
-        
-//         $("#hexagon a img").toggleClass("transparent");
-//         $("#hexagon a").toggleClass("not-active");
-
-//         if (!$("#hexagon a").hasClass("not-active")) {
-//             var xVal = [100, -100, 100, -100];
-//             var yVal = [100, 100, -100, -120];
-//             $("#hexagon a img").each(function( index ) {
-//             $("slide-out").css({
-//                 '-webkit-transform': 'translate(' + xVal[index] + 'px' + ',' + yVal[index] + 'px' + ')',
-//                 '-moz-transform': 'translate(' + xVal[index] + 'px' + ',' + yVal[index] + 'px' + ')',
-//                 '-ms-transform': 'translate(' + xVal[index] + 'px' + ',' + yVal[index] + 'px' + ')',
-//                 '-o-transform': 'translate(' + xVal[index] + 'px' + ',' + yVal[index] + 'px' + ')',
-//                 'transform': 'translate(' + xVal[index] + 'px' + ',' + yVal[index] + 'px' + ')'
-//             });
-//         });
-//         } else {
-//             $("#hexagon img").removeClass("slide-out");
-//         }
-
-//         $("#hexagon img.top").toggleClass("transparent");
-//         $("#hexagon img.bottom").toggleClass("transparent");
-
-//     });
-// });
-
-// function removeSlideClasses(direction) {
-
-//     $("#hexagon img.outline-slide.1").removeClass("slide-out-left");
-//     $("#hexagon img.outline-slide.0").removeClass("slide-out-right");
-//     $("#hexagon a").addClass("not-active");
-//     $("#hexagon img.outline-slide.1").1('otransiti1nd oTransiti1nd msTransiti1nd transiti1nd',
-//         function(e) {
-//             toggleTransparency();
-//         });
-// }
-
-// function slideInOut(hex_1, hex_0) {
-//     if (!$(hex_1).hasClass("slide-out-left")) {
-
-//         $(hex_1).addClass("slide-out-left");
-//         $(hex_0).addClass("slide-out-right");
-//         $("#hexagon a").removeClass("not-active");
-//         toggleTransparency();
-
-//     } else {
-//         removeSlideClasses("out");
-//     }
-// }
